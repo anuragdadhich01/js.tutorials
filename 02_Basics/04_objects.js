f3cef559479501fe0ff0537d53f00f7bee508fe9
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-16 14:59:48 +0000

    Objects

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,37 @@
+//   const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false   
+//   console.log(tinderUser);
+
+const regularUser = {
+    email : "anuragdadhich2005@gmail.com",
+    fullname: {
+        userfullname :{
+            firstname: "Anurag",
+            lastname: "Dadhich"
+        } 
+
+    } 
+    
+
+}
+// console.log(regularUser.fullname?.userfullname);
+
+const obj1 = {1: "a",2:"b"}
+const obj2 = {3: "a",4:"b"}
+const obj3 = {5: "a",6:"b"}
+// console.log(obj1,obj2,obj3);
+
+const users = [
+   {}
+]
+users.email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty("isLogged"));
