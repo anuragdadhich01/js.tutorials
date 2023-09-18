b20317ec8682a7411cb177762a00fb10f5727730
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-18 03:21:47 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,40 @@
+// // switch (key) {
+// //     case value:
+        
+// //         break;
+
+// //     default:
+// //         break;
+// // }
+
+// const month = 3 
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 1:
+//     console.log("Feb");
+//     break;
+//     case 2:
+//     console.log("March");
+//     break;
+//     case 3:
+//     console.log("April");
+//     break;
+//      case 4:
+//         console.log("may");
+//         break;
+
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const isUserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true 
+if (isUserloggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
