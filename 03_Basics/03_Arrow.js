36e0a9d6710bf244463b7b464ba95b1ffa868cd3
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-18 01:27:50 +0000

    =>

--- 03_Basics/03_Arrow.js
+++ 03_Basics/03_Arrow.js
@@ -15,16 +15,27 @@ const user = {
 // function chai() 
 // // {
 // //     let username = "hitesh"
-// // console.log(this);    
-// // }
+// // // console.log(this);    
+// // // }
+// // // chai()
+
+// const chai = function() => {
+//     let username = "Anurag"
+//     // console.log(this.username);
+// }
 // // chai()
 
-const chai = () => {
-    let username = "Anurag"
-    console.log(this.username);
-}
-// chai()
+// const chai =()  => { 
+//     let username = "Anurag"
+//     // console.log(this);
+// }
 
 
-
+// const addTwo = (num1 , num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3 , 4));
      
+// const addTwo = (num1 , num2) =>( num1 + num2)
+const addTwo = (num1 , num2) =>( {username: "Anurag"})
+console.log(addTwo(3 , 4));
