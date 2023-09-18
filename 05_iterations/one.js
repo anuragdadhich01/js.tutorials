7084b6e38eb19a4a52aabe7f73bb4109d61805c7
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-18 04:55:36 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,20 @@
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log("5 is best number");
+    }
+    console.log(element);
+}
+    // console.log(element);
+
+    for (let i = 0; i <= 10; i++) {
+        console.log(`outer loop value: ${i}`);
+      for (let j = 0; j <= 10; j++) {
+    //    console.log(`Inner loo[ value ${j} and inner loop ${i}]`);
+    console.log();
+
+      }
+        
+    }
